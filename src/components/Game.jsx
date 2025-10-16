6332c9f4f7ab6dbb045a4de656249638d7b89a65
DevRudy <55453823+SketchRudy@users.noreply.github.com>
2025-10-16 13:42:53 -0700

    Add initial styles and structure to Game component

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,7 +1,17 @@
+// At least one number
+// At least one array
+// at least one object (current upgrade with name, cost, etc) (stats button)
+
 export default function Game() {
+
+    function Materials() {
+        
+    }
+
     return (
         <>
           <h1>Game Testing</h1>
+
         </>
       )
 }
