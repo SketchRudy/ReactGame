2b6bcf1805ae958527fad82c6af9113b53f133fa
DevRudy <55453823+SketchRudy@users.noreply.github.com>
2025-10-16 14:22:08 -0700

    Add className to images for consistent styling

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -32,13 +32,13 @@ export default function Game() {
         <>
           <h1>Game Testing</h1>
           
-          <img src={Wood} alt="photo of wood" />
+          <img src={Wood} alt="photo of wood" className = "materialimg"/>
           <button onClick={increment}>Wood</button>
           <br></br><br></br>
-          <img src={Rock} alt="photo of rock" />
-          <button>Rock</button>
+          <img src={Rock} alt="photo of rock" className = "materialimg"/>
+          <button>Rock</button> 
           <br></br><br></br>
-          <img src={Metal} alt="photo of metal" />
+          <img src={Metal} alt="photo of metal" className = "materialimg"/>
           <button>Metal</button>
           <br></br><br></br>
 
