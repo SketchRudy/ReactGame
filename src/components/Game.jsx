9f3e62b3a1d9d3e330a92b1d9179f566fd047dc4
ShawnN003 <nguru.shawn@student.greenriver.edu>
2025-10-16 15:57:56 -0500

    Game jsx data implemented

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,14 +3,37 @@
 // at least one object (current upgrade with name, cost, etc) (stats button)
 
 export default function Game() {
+    let woodCounter = 0;
+    let rockCounter = 0;
+    let metalCounter = 0;
+    let playerName = "beginner"
+
+    let totalCount = woodCounter + rockCounter + metalCounter;
+    let resources = {woodCounter,rockCounter,metalCounter}
+
+    const player = {name:playerName, total:totalCount};
+
 
     function Materials() {
         
     }
 
+    function increment(){
+        let [woodCounting, setWood] = React.useState("Yes");
+
+    }
+
     return (
         <>
           <h1>Game Testing</h1>
+          
+          <button onClick={increment}>Wood</button>
+          <br></br><br></br>
+          <button>Rock</button>
+          <br></br><br></br>
+          <button>Metal</button>
+          <br></br><br></br>
+
 
         </>
       )
